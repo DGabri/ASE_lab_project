da1f9f762557b4ef1cdef185112e8187fd86ec81
GabrieleTurchetti <gabriele.turchetti02@gmail.com>
2024-11-12 22:24:56 +0100

    Added all piece API

--- server/piece/app.py
+++ server/piece/app.py
@@ -1,27 +1,80 @@
 from flask import Flask, request, make_response, jsonify
-from DAO import PieceDAOSQLite
+from pieces_DAO import PiecesDAO
+from piece import Piece
 import sqlite3
 
+GRADES = ['C', 'R', 'SR']
+
 app = Flask(__name__)
-piece_dao = PieceDAOSQLite('pieces.db', 'pieces.sql')
+piece_dao = PiecesDAO('pieces.db', 'pieces_scheme.sql')
 
-@app.route('/')
-def index():
-    return make_response(jsonify(s = "res"), 200)
+def piece_is_valid(piece):
+    if not 'name' in piece or not isinstance(piece['name'], str) or not piece['name']:
+        return False
 
-@app.route('/piece', methods=['GET', 'POST'])
-def piece():
-    if request.method == 'POST':
-        data = request.get_json()
-        new_piece = Piece(None, 'King', 'SR', '', 'The king (♔, ♚) is the most important piece in the game of chess. It may move to any adjoining square; it may also perform, in tandem with the rook, a special move called castling. If a player\'s king is threatened with capture, it is said to be in check, and the player must remove the threat of capture immediately. If this cannot be done, the king is said to be in checkmate, resulting in a loss for that player. A player cannot make any move that places their own king in check. Despite this, the king can become a strong offensive piece in the endgame or, rarely, the middlegame.')
-        piece_dao.insert_piece(new_piece)
-        return make_response("Piece created successfully", 201)
-            
-    if request.method == 'GET':
-        piece_id = request.args.get('piece_id')
-        piece_dao = PieceDAOSQLite('pieces.db')
-        pieces = piece_dao.get_piece_by_id(piece_id)
-        return make_response(jsonify(list=pieces), 200)
+    if not 'grade' in piece or not piece['grade'] in GRADES:
+        return False
+
+    if not 'pic_url' in piece or not isinstance(piece['pic_url'], str) or not piece['pic_url']:
+        return False
+
+    if not 'description' in piece or not isinstance(piece['description'], str) or not piece['description']:
+        return False
+
+    return True
+
+@app.route('/piece', methods = ['GET'])
+def get_pieces():
+    data = request.get_json()
+
+    if not 'pieces_id' in data or not all(isinstance(piece_id, int) for piece_id in data['pieces_id']):
+        return make_response(jsonify(message = "Attribute 'piece_id' not found or invalid"), 400)
+
+    pieces, mess = piece_dao.get_pieces_by_id(data['pieces_id'])
+
+    if pieces:
+        return make_response(jsonify(list = pieces), 200)
+    else:
+        return make_response(jsonify(message = str(mess)), 500)
+    
+@app.route('/piece', methods = ['POST'])
+def add_piece():
+    piece = request.get_json()
+
+    if not piece_is_valid(piece):
+        return make_response(jsonify(message = "Attributes not found or invalid"), 400)
+
+    new_piece = Piece(None, piece['name'], piece['grade'], piece['pic_url'], piece['description'])
+    piece_id, mess = piece_dao.insert_piece(new_piece)
+
+    if piece_id:
+        return make_response(jsonify(piece_id = piece_id), 201)
+    else:
+        return make_response(jsonify(message = str(mess)), 500)
+
+@app.route('/piece/<int:piece_id>', methods = ['PUT'])
+def update_piece(piece_id):
+    piece = request.get_json()
+
+    if not piece_is_valid(piece):
+        return make_response(jsonify(message = "Attributes not found or invalid"), 400)
+
+    updated_piece = Piece(piece_id, piece['name'], piece['grade'], piece['pic_url'], piece['description'])
+    updated, mess = piece_dao.update_piece(updated_piece)
+
+    if updated:
+        return make_response(jsonify(message = "Piece updated successfully"), 200)
+    else:
+        return make_response(jsonify(message = mess), 500)
+
+@app.route('/piece/all', methods = ['GET'])
+def get_all_pieces():
+    pieces, mess = piece_dao.get_all_pieces()
+
+    if pieces:
+        return make_response(jsonify(list = pieces), 200)
+    else:
+        return make_response(jsonify(message = mess), 200)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000', debug=True)
--- server/piece/pieces_DAO.py
+++ server/piece/pieces_DAO.py
@@ -1,9 +1,8 @@
 import sqlite3
-import typing
 
 class PiecesDAO:
     def __init__(self, database, scheme):
-        self.connection = sqlite3.connect(database)
+        self.connection = sqlite3.connect(database, check_same_thread = False)
         self.cursor = self.connection.cursor()
 
         with open(scheme, 'r') as sql_file:
@@ -12,51 +11,54 @@ class PiecesDAO:
         try:
             with self.connection:
                 self.cursor.executescript(sql_script)
-        except sqlite3.Error:
-            pass # handle exception
+        except sqlite3.Error as e:
+            print(f"Cannot initialize pieces DAO: {e}")
     
-    def get_piece_by_id(self, piece_id) -> Any:
+    def get_pieces_by_id(self, pieces_id):
         try:
-            self.cursor.execute('SELECT * FROM pieces WHERE id = ?', (piece_id))
-            return cursor.fetchone()
-        except sqlite3.Error:
-            self.connection.rollback()
-            return None # check structure of return data
+            with self.connection:
+                self.cursor.execute('SELECT * FROM pieces WHERE id IN (%s)' % ','.join('?' * len(pieces_id)), pieces_id)
+
+            return self.cursor.fetchall(), ""
+        except sqlite3.Error as e:
+            return None, e
     
-    def get_all_pieces(self) -> Any:
+    def get_all_pieces(self):
         try:
-            self.cursor.execute('SELECT * FROM pieces')
-            return cursor.fetchall()
-        except sqlite3.Error:
-            self.connection.rollback()
-            return None # check structure of return data
+            with self.connection:
+                self.cursor.execute('SELECT * FROM pieces')
+
+            return self.cursor.fetchall(), ""
+        except sqlite3.Error as e:
+            return None, e
     
-    def insert_piece(self, piece) -> int:
+    def insert_piece(self, piece):
         try:
-            self.cursor.execute('INSERT INTO pieces (name, grade, pic_url, description) VALUES (?, ?, ?, ?)', (piece.name, piece.grade, piece.pic_url, piece.description))
-            self.connection.commit()
-            return 0 # id of the created piece
-        except sqlite3.Error:
-            self.connection.rollback()
-            return 0
+            self.cursor.execute('INSERT INTO pieces (name, grade, pic_url, description) VALUES (?, ?, ?, ?) RETURNING id', (piece.name, piece.grade, piece.pic_url, piece.description))
+            row = self.cursor.fetchone()
+            piece_id = row if row else None
+            self.cursor.commit()
+            return piece_id, ""
+        except sqlite3.Error as e:
+            return None, e
+
+    def update_piece(self, piece):
+        try:
+            with self.connection:
+                self.cursor.execute('UPDATE pieces SET name = ?, grade = ?,  pic_url = ?,  description = ? WHERE id = ?', (piece.name, piece.grade, piece.pic_url, piece.description, piece.id))
+            
+            return True, ""
+        except sqlite3.Error as e:
+            return False, e
     
-    def update_piece(self, piece) -> bool:
+    def delete_piece(self, piece_id):
         try:
-            self.cursor.execute('UPDATE pieces SET name = ?, grade = ?,  pic_url = ?,  description = ?WHERE id = ?', (piece.name, piece.grade, piece.pic_url, piece.description))
-            self.connection.commit()
-            return True
-        except sqlite3.Error:
-            self.connection.rollback()
-            return False
-    
-    def delete_piece(self, piece_id) -> bool:
-        try:
-            self.cursor.execute('DELETE FROM pieces WHERE id = ?', (piece_id))
-            self.connection.commit()
-            return True
-        except sqlite3.Error:
-            self.connection.rollback()
-            return False
+            with self.connection:
+                self.cursor.execute('DELETE FROM pieces WHERE id = ?', (piece_id))
+
+            return True, ""
+        except sqlite3.Error as e:
+            return False, e
 
     def close(self):
         self.connection.close()
