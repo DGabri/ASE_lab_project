5524db8477e74f1166dae173d181bd434e4627a1
Massimo Parlanti <124680396+massiparla@users.noreply.github.com>
2024-11-27 19:15:22 +0100

    Update temp_auction.py

--- server/auction/temp_auction.py
+++ server/auction/temp_auction.py
@@ -4,7 +4,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 import pytest
 import json
-from app import app  # Import your Flask app (the one you've defined in your script)
+from app import app 
 
 @pytest.fixture
 def client():
